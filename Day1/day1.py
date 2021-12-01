4d94713b4a7963b2068c4a115154ddcacafd891f
Raghav Agarwal <raghav@ea.com>
2021-12-01 22:43:23 +0000

    Part 2 complete

--- Day1/day1.py
+++ Day1/day1.py
@@ -5,7 +5,7 @@ def getInput():
     input = []
     with open(os.path.join(sys.path[0], "input.txt")) as file_in:
         for line in file_in:
-            input.append(int(line.strip()))        
+            input.append(int(line.strip()))
     return input
 
 def detectDepthIncrease(input):
@@ -22,8 +22,31 @@ def detectDepthIncrease(input):
 
     return depth_increase_count
 
+def detectDepthIncreaseSlidingWindow(input):
+    prevRunningSum = input[0] + input[1] + input[2]
+    print(prevRunningSum)
+    depth_increase_count = 0 
+    for idx, value in enumerate(input[1:-1]):
+        currentRunningSum = input[idx] + input[idx+1] + input[idx+2]
+        print("currentRunningSum: {0}".format(currentRunningSum))
+        if currentRunningSum > prevRunningSum:
+            depth_increase_count+=1
+            print("Increased: prevRunningSum: {0}, currentRunningSum: {1}".format(prevRunningSum, currentRunningSum))
+        else:
+            print("Unchanged: prevRunningSum: {0}, currentRunningSum: {1}".format(prevRunningSum, currentRunningSum))
+
+        prevRunningSum = currentRunningSum
+
+    return depth_increase_count
+
 if __name__ == "__main__":
     input = getInput()
-    result = detectDepthIncrease(input)
-    print("Depth Measurement Increases: {0} times".format(result))
+    print(input)
+    #Part 1
+    # result = detectDepthIncrease(input)
+    # print("Depth Measurement Increases: {0} times".format(result))
+
+    #Part 2 
+    result = detectDepthIncreaseSlidingWindow(input)
+    print("Depth Measurement Increases (Three Measurements Sliding Window): {0} times".format(result))
 
